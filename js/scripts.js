a0b5d4d60d63c75d9c5947d22a29f1adf2f65d5f
Mirosław Drozd <51049107+TrevorMk@users.noreply.github.com>
2021-02-15 09:53:38 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+
+const timeInetrval = setInterval(function() {       
+  const giveawayEndDate = new Date("Feb 14, 2021 00:00:00").getTime();
+  const timeDistance = giveawayEndDate - Date.now();
+
+  const days = Math.floor(timeDistance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDistance / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((timeDistance / 1000 / 60) % 60);
+  const seconds = Math.floor((timeDistance / 1000) % 60);
+
+  document.getElementById("countdown");
+  countdown.querySelector('.clock__days').innerHTML = ('0' + days).slice(-2);
+  countdown.querySelector('.clock__hours').innerHTML = ('0' + hours).slice(-2);
+  countdown.querySelector('.clock__minutes').innerHTML = ('0' + minutes).slice(-2);
+  countdown.querySelector('.clock__seconds').innerHTML = ('0' + seconds).slice(-2);
+  
+  
+  if (timeDistance < 0) {
+    clearInterval(timeInetrval);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+}
+
+  
